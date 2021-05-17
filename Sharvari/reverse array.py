b932dac49f0607cc6794509c77976f33c926e6f4
Sharvari Sanjeev Potnis <potnissp@amazon.com>
2021-05-17 00:15:01 -0400

    xyz

---
+++ Sharvari/reverse array.py
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2513
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red109\green109\blue109;\red32\green32\blue32;\red153\green168\blue186;
+\red86\green132\blue173;\red191\green100\blue38;\red117\green114\blue185;}
+{\*\expandedcolortbl;;\csgenericrgb\c42745\c42745\c42745;\csgenericrgb\c12549\c12549\c12549;\csgenericrgb\c60000\c65882\c72941;
+\csgenericrgb\c33725\c51765\c67843;\csgenericrgb\c74902\c39216\c14902;\csgenericrgb\c45882\c44706\c72549;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs26 \cf2 \cb3 #Array reversal\
+\
+\cf4 x=[\cf5 1\cf6 ,\cf5 2\cf6 ,\cf5 3\cf6 ,\cf5 4\cf4 ]\
+x=x[::-\cf5 1\cf4 ]\
+\cf7 print\cf4 (x)\
+\
+}
