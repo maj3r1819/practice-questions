5f218479f1d108c0aae30c1eea202679272ac831
Sagar <major1819@gmail.com>
2021-07-14 21:57:47 +0530

    solved using dictionary

--- Sagar/Top Interview Question (Easy)/Array/Single_number.py
+++ Sagar/Top Interview Question (Easy)/Array/Single_number.py
@@ -1,6 +1,36 @@
-nums = [4, 1, 2, 1, 2]
-i = 0
-while(i < len(nums)):
-    j = i+1
+"""
+Given a non - empty array of integers nums, every element appears twice except for one.Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example
+1:
+
+Input: nums = [2, 2, 1]
+Output: 1
+Example
+2:
+
+Input: nums = [4, 1, 2, 1, 2]
+Output: 4
+Example
+3:
+
+Input: nums = [1]
+Output: 1
+"""
 
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        frequency = {}
+        for num in nums:
+            if num not in frequency:
+                frequency[num] = 1
+
+            else:
+                frequency[num] += 1
+
+        for i in range(len(nums)):
+            if frequency[nums[i]] == 1:
+                return nums[i]
